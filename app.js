7c8f340cd0dd61921a3d37556cf33553fc1ab0df
Bima Santrya <hafidhbaru@gmail.com>
2022-04-23 15:59:31 +0700

    finish nodeJS basics

--- app.js
+++ app.js
@@ -2,24 +2,11 @@
 
 const http = require('http')
 
+const reqHandler = require('./routes')
 const PORT = 4000
 
-// callback function that take our requrest and response for later used on http createServer
-function rqListener(req, res) {
-  const url = req.url
-  if (url === '/') {
-    res.write('<html>')
-    res.write('<head></head>')
-    res.write('<body><form action="/message" method="POST"><input type="text"><button>Click</button></form></body>')
-    res.write('</html>')
-    return res.end()
-  } else {
-    res.setHeader('Content-Type', 'text/html')
-    res.write('this is json saying hello')
-    res.end()
-  }
-}
+const server = http.createServer(reqHandler)
 
-const server = http.createServer(rqListener)
-
-server.listen(PORT)
+server.listen(PORT, 'localhost', () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
