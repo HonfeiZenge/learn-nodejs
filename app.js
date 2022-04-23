c28bb18b23e55f1b2c6d928286256d95aa517d78
Bima Santrya <hafidhbaru@gmail.com>
2022-04-23 16:42:46 +0700

    finish nodeJS basics assignment

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 'use strict'
 
 const http = require('http')
+const PORT = 3000
+const host = 'localhost'
 
-const reqHandler = require('./routes')
-const PORT = 4000
+const server = http.createServer((req, res) => {
+  const url = req.url
+  const method = req.method
 
-const server = http.createServer(reqHandler)
+  if (url === '/') {
+    let template = `
+    <form action="/create-user" method="POST">
+      <input type="text" name="username">
+      <button>Add</button>
+    </form>
+    `
+    res.write('<h1>This is Node JS Assignment</h1>')
+    res.write(template)
+    return res.end()
+  }
+  if (url === '/users') {
+    let template =  `
+    <html>
+    <head><title>Node JS Assignment</title></head>
+    <body>
+      <ul>
+        <li>Kaguya</li>
+        <li>Inuyasha</li>
+        <li>Kogane</li>
+      </ul>
+    </body>
+    </html>
+    `
+    res.write(template)
+    return res.end()
+  }
+  if (url === '/create-user' && method === 'POST') {
+    const body = []
+    req.on('data', (chunk) => {
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString()
+      const username = parsedBody.split('=')[1]
+      console.log(username)
+      res.statusCode = 302
+      res.setHeader('Location', '/')
+      return res.end()
+    })
+  }
+})
 
-server.listen(PORT, 'localhost', () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+server.listen(PORT, host, () => {
+  console.log(`listening to http://${host}:${PORT}`)
 })
